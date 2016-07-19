babc9fe5b49448dc4cb68a4cfb201da55c40345a
Lukasz Gasior <lukasz@gasior.net.pl>
2016-07-19 23:08:28 +0200

    simple UI for inserting coins

--- js/app.js
+++ js/app.js
@@ -1,5 +1,29 @@
+var Quarter = (function () {
+    function Quarter() {
+        this.value = .25;
+    }
+    Object.defineProperty(Quarter.prototype, "Value", {
+        get: function () {
+            return this.value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Quarter.prototype.getImageUrl = function () {
+        return "img/Quarter.png";
+    };
+    return Quarter;
+}());
+/// <reference path="coin.ts" />
 var VendingMachine = (function () {
     function VendingMachine() {
+        var _this = this;
+        this.paid = 0;
+        this.acceptCoin = function (coin) {
+            _this.paid += coin.Value;
+            var element = document.getElementById("total");
+            element.innerHTML = _this.paid.toString();
+        };
     }
     return VendingMachine;
 }());
--- ts/vendingMachine.ts
+++ ts/vendingMachine.ts
@@ -1,3 +1,12 @@
-class VendingMachine {
+/// <reference path="coin.ts" />
 
+class VendingMachine {
+    private paid: number = 0;
+
+    acceptCoin = (coin: Quarter): void => {
+        this.paid += coin.Value;
+
+        var element = document.getElementById("total");
+        element.innerHTML = this.paid.toString();
+    }
 }
